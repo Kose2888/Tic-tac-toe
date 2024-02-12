7392356ff48326d1950b28eaf6cf70aa1aaaf7b5
sysassist <sysassist@mailhost.cs.uleth.ca>
2024-02-12 15:54:43 -0700

    File setup

---
+++ test/test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "gtest/gtest.h"
+
+class Test : public testing::Test {
+  protected:
+    //uint8_t *buffer;
+
+    void SetUp() override {
+    }
+
+    void TearDown() override {
+    }
+};
+
+TEST(Test, nodeTest){
+}
